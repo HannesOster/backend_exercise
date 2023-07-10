2e0be5ed4a7e23d28ff1704c82ae8628d017bcc9
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-10 11:35:49 +0200

    add greeting and count

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ import { createServer } from "node:http";
 function firstFish() {
   return "<h1>Shrimp<h1>";
 }
-function About() {}
+let count = 0;
+function Greeting(count) {
+  const name = "Johannes";
+  const age = "29";
+  const profession = "web developer";
+  return `<h2>Hey, my name is ${name}, i am ${age} years old and i am a ${profession}.</h2>
+  <p>This page was visited ${count} times.</p>`;
+}
 
 const server = createServer((request, response) => {
   console.log("Request incoming...", request.url);
@@ -12,6 +19,10 @@ const server = createServer((request, response) => {
   }
   if (request.url === "/api/fish2") {
     response.end("Anemonenfisch");
+  }
+  if (request.url === "/") {
+    count++;
+    response.end(Greeting(count));
   }
 });
 
